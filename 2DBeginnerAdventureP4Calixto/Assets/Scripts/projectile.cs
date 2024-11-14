89cbccf96596d5118bd6835f8040889d933efae0
BETA-TESTER-FOR-AINCRAD <sammycalixto21@gmail.com>
2024-11-14 11:44:15 -0800

    oki
    pookie

---
+++ 2DBeginnerAdventureP4Calixto/Assets/Scripts/projectile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
